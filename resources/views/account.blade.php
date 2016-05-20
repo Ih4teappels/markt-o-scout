5854fbc25ff42be45a3cec3aaff80084b479723b
Robinwist <robinwist@hotmail.com>
2016-05-20 17:07:02 +0200

    Account page now working properly

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,10 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+
 <?php
 
-        $results = DB::select('select * from users where name = :name', ['name' => 'Robin']);
-
-                echo json_encode($results);
+        $users = DB::select('SELECT * FROM users WHERE id = :id', ['id' => 2]);
+        foreach ($users as $user){
+                echo $user->username;
+                echo $user->name;
+                echo $user->email;
+        }
 ?>
+
 @endsection
