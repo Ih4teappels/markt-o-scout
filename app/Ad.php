be9fd00fa7da8e1efc580c0cb569aafcaecae1ac
Robinwist <robinwist@hotmail.com>
2016-05-26 11:42:55 +0200

    -

---
+++ app/Ad.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ad extends Model
+{
+
+    protected $fillabe = [
+        'product_name', 'product_desc', 'city', 'price'
+    ];
+}
