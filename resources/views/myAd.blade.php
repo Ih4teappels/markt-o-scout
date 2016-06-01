08298aabd05ee2612076092239304152025a33e0
Robinwist <robinwist@hotmail.com>
2016-06-01 10:49:30 +0200

    -

--- resources/views/myAd.blade.php
+++ resources/views/myAd.blade.php
@@ -1,36 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<<<<<<< HEAD
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
                     <div class="panel-heading">My ads</div>
-=======
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">My ads</div>
->>>>>>> 2304ec06324e79044fd237b4d6b0477ab1723089
 
                     <div class="panel-body">
 
-<<<<<<< HEAD
                         @foreach($oneAd as $ad)
 
                             {{ $ad->product_name }}
 
                         @endforeach
                     </div>
-=======
-                    @foreach($oneAd as $ad)
-
-                    {{ $ad->product_name }}
-                    
-                    @endforeach
->>>>>>> 2304ec06324e79044fd237b4d6b0477ab1723089
                 </div>
             </div>
         </div>
